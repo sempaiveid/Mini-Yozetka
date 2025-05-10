57b3c16ac22224e39937f39a301473f4b7d171ff
sempaiveid <vladuk640@gmail.com>
2025-05-11 02:19:28 +0300

    adds header

---
+++ src/app/components/header/header-input/header-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header-input',
+  imports: [],
+  templateUrl: './header-input.component.html',
+  styleUrl: './header-input.component.css'
+})
+export class HeaderInputComponent {
+
+}
