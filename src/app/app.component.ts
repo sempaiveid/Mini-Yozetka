5fd3b54a80d03bd18f1460f43a5df62e43dab5d8
teoto4 <teoto@ukr.net>
2025-05-11 02:22:20 +0300

    "добавлен сервис для корзины"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component} from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CartService } from './services/cart.service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,],
@@ -7,9 +8,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
- constructor(){
+  cart: CartService = inject(CartService);
   
- }
+   setNewProd(){
+    this.cart.productCart = {name : "gamePad", price: 24};
+   } 
+  
+   constructor(){
+    this.setNewProd();
+    console.log(this.cart.productCart);
+  }
  
 }
