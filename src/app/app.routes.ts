e6d5f460e5bc6b402768b09adb13e58f6b5de248
teoto4 <teoto@ukr.net>
2025-05-17 00:22:56 +0300

    Исправил импорты и пути к файлам админки

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
-import { AppComponent } from './app.component';
-import { AdminPageComponent } from './pages/admin-page/admin-page.component'; // Убедитесь, что путь к компоненту правильный
-import { LoginPageComponent } from './pages/login-page/login-page.component'; // Убедитесь, что путь к компоненту правильный
-import { ProfileComponent } from './pages/profile/profile.component'; // Убедитесь, что путь к компоненту правильный
-import { ProductPageComponent } from './components/product-page/product-page.component';
+import { LoginPageComponent } from './pages/admin-page/login-page/login-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component'; 
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/admin-page/profile/profile.component';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
