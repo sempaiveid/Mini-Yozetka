d3b5b6828fbdf476c7b0157e8cc2a93d7312d804
MExAnIk20 <khrustalevs098@gmail.com>
2025-05-16 18:06:30 +0300

    I did some work with the footer and made a sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CartService } from './services/cart.service';
+import { CategorySidebarComponent } from './components/category-sidebar/category-sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent, CategorySidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
