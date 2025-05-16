412b5cdec4663b9c493083a3ff6ac65f9785307a
teoto4 <teoto@ukr.net>
2025-05-17 00:09:47 +0300

    Сверстал компонент профиля, добавил нужные компоненты в админку

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
+import { ProfileComponent } from './pages/admin-page/profile/profile.component';
+import { LoginPageComponent } from './pages/admin-page/login-page/login-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"admin",
+        component: AdminPageComponent,
+        title:"Admin",
+        children:[
+            {   
+                path: "login",
+                component: LoginPageComponent,
+                title: "Login"
+            },
+            {
+                path: "profile",
+                component: ProfileComponent,
+                title: "Profile"
+            }
+        ]
+    }
+];
--- src/app/pages/admin-page/admin-page.component.ts
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-page',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.css'
 })
 export class AdminPageComponent {
-
+  router = inject(Router);
+  logined:boolean = false;
+  constructor(){
+    if (!this.logined) {
+      this.router.navigate(["login"])
+    } else{
+      this.router.navigate(["profile"])
+    }
+  }
 }
