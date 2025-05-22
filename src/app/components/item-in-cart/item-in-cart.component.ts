321098b523ef99613c3a1dbe4572c9cde71d50ed
sempaiveid <vladuk640@gmail.com>
2025-05-22 23:51:30 +0300

    adds coutn in cart

--- src/app/components/item-in-cart/item-in-cart.component.ts
+++ src/app/components/item-in-cart/item-in-cart.component.ts
@@ -2,18 +2,27 @@ import { Component, inject } from '@angular/core';
 import { PoputMenuComponent } from './poput-menu/poput-menu.component';
 import { CounterProductComponent } from './counter-product/counter-product.component';
 import { ProductService } from '../../services/product.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-item-in-cart',
-  imports: [PoputMenuComponent, CounterProductComponent, NgFor, RouterModule,],
+  imports: [PoputMenuComponent, CounterProductComponent, NgFor, RouterModule, NgIf],
   templateUrl: './item-in-cart.component.html',
   styleUrl: './item-in-cart.component.css'
 })
 export class ItemInCartComponent {
+  cartTemplate: HTMLElement
   product: CartService = inject(CartService)
   productCard = this.product.productCart
   inputCount = 1
+  clearCart() {
+    this.product.resetCart()
+    this.productCard = []
+
+  }
+  constructor() {
+    this.cartTemplate = document.querySelector(".main-cart") as HTMLElement
+  }
 }
