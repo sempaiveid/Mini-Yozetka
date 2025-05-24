06e82cd427c0c43d6dbf5bb2318302ed53de7e4b
Yevhen Zubko <zubkoevgene@gmail.com>
2025-05-24 12:43:44 +0300

    creating a 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/admin-page/profile/profile.component';
 import { Routes } from '@angular/router';
 import { ProductComponent } from './pages/product/product.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
     {
@@ -38,5 +39,10 @@ export const routes: Routes = [
         path: "product",
         component:  ProductComponent,
         title: "Product"
-    }
+    },
+    {
+        path: '**',
+        component: NotFoundPageComponent,
+        title: 'Page not found'
+      }
 ];
