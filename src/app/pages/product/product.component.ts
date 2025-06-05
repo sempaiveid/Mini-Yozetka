1c36593eb9946e00d74fe9b2c8ab091f4db31d36
sempaiveid <vladuk640@gmail.com>
2025-06-06 02:41:16 +0300

    add in title name product

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -2,16 +2,17 @@ import { Component, inject, Input } from '@angular/core';
 import { CommitProductComponent } from './commit-product/commit-product.component';
 import { ProductService } from '../../services/product.service';
 import { NgIf } from '@angular/common';
-import { ActivatedRoute ,Router} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HeaderCatergoryComponent } from './header-catergory/header-catergory.component';
 import { ProductImageComponent } from './product-image/product-image.component';
 import { TilePriceComponent } from './tile-price/tile-price.component';
 import { ProductTileDataComponent } from './product-tile-data/product-tile-data.component';
+import { Title } from '@angular/platform-browser';
 
 
 @Component({
   selector: 'app-product',
-  imports: [CommitProductComponent, NgIf,HeaderCatergoryComponent, ProductImageComponent, TilePriceComponent,ProductTileDataComponent],
+  imports: [CommitProductComponent, NgIf, HeaderCatergoryComponent, ProductImageComponent, TilePriceComponent, ProductTileDataComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
@@ -21,6 +22,7 @@ export class ProductComponent {
   activeRoute = inject(ActivatedRoute);
   router = inject(Router)
   products = this.productItems.get()
+  titleService = inject(Title);
 
   item:
     | {
@@ -29,16 +31,19 @@ export class ProductComponent {
       picture: string;
       price: number;
       category: string;
-      discribe :string;
+      discribe: string;
     }
     | undefined;
 
   ngOnInit() {
     const id = this.activeRoute.snapshot.params['id'];
     this.item = this.productItems.get().find(item => item.id === id);
-  
+
     if (!this.item) {
       this.router.navigate(['/404']);
+    }
+    else {
+      this.titleService.setTitle(this.item.name)
     }
 
   }
