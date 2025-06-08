cb97860e24e6abf7cfbcbb135c33a8dd357769c0
Marina-Syl <marina.bankovskaya18@gmail.com>
2025-06-09 01:25:06 +0300

    added css for product component

--- src/app/pages/product/tile-price/tile-price.component.ts
+++ src/app/pages/product/tile-price/tile-price.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Event } from '@angular/router';
+import { Component, inject, Input } from '@angular/core';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-tile-price',
@@ -7,6 +9,36 @@ import { Component, Input } from '@angular/core';
   styleUrl: './tile-price.component.css'
 })
 export class TilePriceComponent {
-@Input() name:string = ""
-@Input()price:number = 0
+  productSet: CartService = inject(CartService);
+  
+
+  @Input() name: string = ""
+  @Input() price: number = 0
+  @Input() product: any;
+
+  booleanText = true;
+
+  addToCart(product: any) {
+    let isCart: any = this.productSet.productCart.some((el: any) => el.id === product.id)
+    if (!isCart) {
+      this.productSet.productCart = product;
+    }
+    else {
+      this.productSet.deleteItem(product.id)
+    }
+
+  }
+  buttonClick(button: HTMLElement): void {
+   
+    let text = button.querySelector(".buyTxt");
+    this.booleanText = !this.booleanText
+
+    if (!this.booleanText) {
+      text!.textContent = "Додано в корзину";
+    } else if (this.booleanText) {
+      text!.textContent = "Купити";
+    }
+//localStorage.clear()
+  }
 }
+
