1fcd84589f22b622b4e127e155517687b23ca29a
teoto4 <teoto@ukr.net>
2025-06-15 01:53:02 +0300

    Добавил конвертацию к основной валюте, так же исправил стили для добавленных товаров, инпутов и другие мелкие правки в css

--- src/app/pages/admin-page/profile/profile.component.ts
+++ src/app/pages/admin-page/profile/profile.component.ts
@@ -20,13 +20,22 @@ export class ProfileComponent {
     name_product : ['', [Validators.required, Validators.pattern(/^[A-Za-zА-Яа-яЁё\s]+$/), Validators.minLength(4)]],
     image_product: ['', [Validators.required, Validators.pattern(/^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|webp|svg))$/i)], [imageExistsValidator()]],
     description_product: ['', [Validators.required, Validators.pattern(/^[A-Za-zА-Яа-яЁё0-9\s]+$/), Validators.minLength(5)]],
-    price_product:[ 0 , [Validators.required, Validators.pattern(/^\d+$/), Validators.pattern(/^[1-9][0-9]*$/)]],
+    price_product:[ null , [Validators.required, Validators.pattern(/^[1-9][0-9]*$/)]],
     category_product:['', [Validators.required]],
+    currency_price_product:['', [Validators.required]]
   })
+  
+  dollar: number = 41.30;
+  euro: number = 47.72;
+  
 
   user_products:Product[] = [];
 
   productToDelete: string | null = null;
+
+  ngOnInit(){
+    this.addProductForm.get('currency_price_product')?.setValue('hryvnia');
+  }
 
   constructor(){
     this.update_user_products();
@@ -49,16 +58,26 @@ export class ProfileComponent {
       id: '',
       name: this.addProductForm.controls.name_product.value as string,
       picture: this.addProductForm.controls.image_product.value as string,
-      price: this.addProductForm.controls.price_product.value as number,
+      price: 0 as number,
       category:this.addProductForm.controls.category_product.value as string,
       description: {text: this.addProductForm.controls.description_product.value as string} as object,
       uploader: this.loginService.user.login as string,
     };
+    console.log(this.addProductForm.controls.currency_price_product.value);
+
+    const price = Number(this.addProductForm.controls.price_product.value);
+    switch(this.addProductForm.controls.currency_price_product.value){
+      case "hryvnia": product.price = price as number; break;
+      case "dollar": product.price = price as number *  Math.ceil(this.dollar) as number; break;
+      case "euro": product.price = price *  Math.ceil(this.euro) as number; break;
+      default: throw new Error('Неверная валюта выбрана');
+    }
 
     this.productService.addProduct = product;
     this.loginService.add_user_product(this.productService.find_uploader(product.uploader));
     this.addProductForm.reset();
     this.update_user_products();
+    this.addProductForm.get('currency_price_product')?.setValue('hryvnia');
   }
 
   confirmDelete(productId: string) {
