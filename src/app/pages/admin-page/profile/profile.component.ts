3050733dd9dc5b44d8f374c0add4eb29fedb95d2
teoto4 <teoto@ukr.net>
2025-06-23 20:19:27 +0300

    Мелкий фикс валидатора описания

--- src/app/pages/admin-page/profile/profile.component.ts
+++ src/app/pages/admin-page/profile/profile.component.ts
@@ -19,7 +19,7 @@ export class ProfileComponent {
   addProductForm = new FormBuilder().group({
     name_product : ['', [Validators.required, Validators.pattern(/^[A-Za-zА-Яа-яЁё\s]+$/), Validators.minLength(4)]],
     image_product: ['', [Validators.required], [imageExistsValidator()]],
-    description_product: ['', [Validators.required, Validators.pattern(/^[A-Za-zА-Яа-яЁё0-9\s]+$/), Validators.minLength(5)]],
+    description_product: ['', [Validators.required, Validators.pattern(/^[A-Za-zА-Яа-яІіЇїЄєҐґЁё0-9\s]+$/), Validators.minLength(5)]],
     price_product:[ null , [Validators.required, Validators.pattern(/^[1-9][0-9]*$/)]],
     category_product:['', [Validators.required]],
     currency_price_product:['', [Validators.required]]
