4e8f84f818bc0f188fd9fe2e20cad9242b4e6930
sempaiveid <vladuk640@gmail.com>
2025-07-04 17:11:30 +0300

    Переписал подгрузку дефолт товаров

--- src/app/components/item-in-cart/item-in-cart.component.ts
+++ src/app/components/item-in-cart/item-in-cart.component.ts
@@ -6,32 +6,50 @@ import { NgFor, NgIf } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { CurrencyPipe } from '../../pipes/currency-convert.pipe';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-item-in-cart',
-  imports: [PoputMenuComponent, CounterProductComponent, NgFor, RouterModule, NgIf, CurrencyPipe],
+  imports: [
+    PoputMenuComponent,
+    CounterProductComponent,
+    NgFor,
+    RouterModule,
+    NgIf,
+    CurrencyPipe,
+  ],
   templateUrl: './item-in-cart.component.html',
-  styleUrl: './item-in-cart.component.css'
+  styleUrl: './item-in-cart.component.css',
 })
 export class ItemInCartComponent {
-  cartTemplate: HTMLElement
-  product: CartService = inject(CartService)
-  productCard = this.product.productCart
-  inputCount = 1
-  isBuy = false
+  cartTemplate: HTMLElement;
+  product: CartService = inject(CartService);
+  productCard : any =[];
+  productSet: any = this.product.productCart;
+  inputCount = 1;
+  isBuy = false;
+  http = inject(HttpClient);
+  loadCart() {
+    this.http.get<any[]>('http://localhost:3000/cart').subscribe((data)=>{
+      this.productCard = data
+      console.log(data)
+    });
+  }
 
   buy() {
-    this.product.resetCart()
-    this.productCard = []
-    this.isBuy = true
-    setTimeout(() => this.isBuy = false,1500)
+    this.product.resetCart();
+    this.productCard = [];
+    this.isBuy = true;
+    setTimeout(() => (this.isBuy = false), 1500);
   }
   clearCart() {
-    this.product.resetCart()
-    this.productCard = []
-
+    this.product.resetCart();
+    this.productCard = [];
   }
   constructor() {
-    this.cartTemplate = document.querySelector(".main-cart") as HTMLElement
+    this.cartTemplate = document.querySelector('.main-cart') as HTMLElement;
+  }
+  ngOnInit() {
+    this.loadCart();
   }
 }
